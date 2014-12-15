3bc0b3d396fb06b9a306c27ac239d5f1d56776f9
Peace_Mac <wangyclove@gmail.com>
2014-12-14 20:44:45 -0800

    updated mountainPoint

--- src/com/practice/MountainPoint.java
+++ src/com/practice/MountainPoint.java
@@ -1,5 +1,19 @@
 package com.practice;
 
 public class MountainPoint {
-
+	public static void main(String[] args) {
+		int[][] board = {
+				{1,2,3,4,5,6},
+				{4,5,6,7,6,8},
+				{4,5,6,8,9,0},
+				{8,7,6,5,4,3},
+				{4,5,7,2,9,2}
+		};
+		
+		findMountain(board);
+	}
+	
+	public static void findMountain(int[][] board) {
+		
+	}
 }
