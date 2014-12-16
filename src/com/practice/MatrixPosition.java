2d1d091c04d6a75d734d6e2293d769e07fe67c98
Peace_Mac <wangyclove@gmail.com>
2014-12-16 15:03:45 -0800

    finished MatrixPosition

--- src/com/practice/MatrixPosition.java
+++ src/com/practice/MatrixPosition.java
@@ -1,25 +1,103 @@
 package com.practice;
 
-/**
- * 25.     Matrix Position
-Given an NxN matrix with unique integers: Find and print positions of all numbers such that it is the biggest in its row and also the smallest in its column . e.g. : In 3 x 3 with elements //写一个坐标类 写出他的equal函数，求两个list的交集（自己写）
-1 2 3 
-4 5 6 
-7 8 9 .          
-the number is 3 and position (1,3)
-
- * @author Peace
- *
- */
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class MatrixPosition {
 	public static void main(String[] args) {
-		
+		int[][] matrix = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+		findPosition(matrix);
 	}
 	
-	
+	public static void findPosition(int[][] matrix) {
+		//find the maximum point in each row
+		ArrayList<PointwithValue> rowMax = new ArrayList<PointwithValue>();
+		for (int i = 0; i < matrix.length; i++) {
+			int val = matrix[i][0];
+			int x = i;
+			int y = 0;
+			for (int j = 1; j < matrix[0].length; j++) {
+				if (matrix[i][j] > val) {
+					val = matrix[i][j];
+					x = i;
+					y = j;
+				}
+			}
+			rowMax.add(new PointwithValue(val, x+1, y+1));
+		}
+		
+		//find the minimum point in each col
+		ArrayList<PointwithValue> colMin = new ArrayList<PointwithValue>();
+		for (int j = 0; j < matrix[0].length; j++) {
+			int val = matrix[0][j];
+			int x = 0;
+			int y = j;
+			for (int i = 1; i < matrix.length; i++) {
+				if (matrix[i][j] < val) {
+					val = matrix[i][j];
+					x = i;
+					y = j;
+				}
+			}
+			colMin.add(new PointwithValue(val, x+1, y+1));
+		}
+		
+		// find out the common elements in rowMax and colMin
+		for (PointwithValue p : rowMax) {
+			for (PointwithValue q: colMin) {
+				System.out.println(p + ", " + q);
+				System.out.println(p.equals(q));
+				if (p.equals(q)) {
+					System.out.println(p.val + "(" + p.x + ", " + p.y + ")");
+					colMin.remove(q);
+					break;
+				}
+			}
+		}
+	}
 }
 
-class Point{
+class PointwithValue {
+	int val;
+	int x;
+	int y;
 	
+	public PointwithValue(int val, int x, int y) {
+		this.val = val;
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return Objects.hash(this.val, this.x, this.y);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		
+		if (!(obj instanceof PointwithValue)) {
+			return false;
+		}
+		
+		PointwithValue p = (PointwithValue)obj;
+		if (this.val != p.val || this.x != p.x || this.y != p.y) {
+			return false;
+		}
+		
+		return true;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return val + " " + x + " " + y;
+	}
 }
