d57f764992b535e0769fbdede607ceb7e44a082f
wangyclove <wangyclove@gmail.com>
2014-12-16 16:47:29 -0800

    add comments on colorful and wellordered

--- src/com/practice/Colorful.java
+++ src/com/practice/Colorful.java
@@ -7,24 +7,40 @@ public class Colorful {
 	public static void main(String[] args) {
 		int testNum = 263;
 		int testNum2 = 236;
-		System.out.println("testNum is Colorful " + isColorful(testNum));
-		System.out.println("testNum2 is Colorful " + isColorful(testNum2));
+		System.out.println(testNum + " is Colorful " + isColorful(testNum));
+		System.out.println(testNum2 + " is Colorful " + isColorful(testNum2));
 	}
 	
 	public static boolean isColorful(int num) {
 		ArrayList<Integer> digits = new ArrayList<Integer>();
 		HashSet<Integer> set = new HashSet<Integer>();
-		
+		if (num < 0) {
+			return false;
+		}
 		while (num > 0) {
-			digits.add(num%10);
+			digits.add(0, num%10);
 			num /= 10;
 		}
+		System.out.println(digits.size());
 		
-		for (int i = 1; i <= digits.size(); i++) {
-			for (int j = 0; j < digits.size() - i; j++) {
-				 
+		for (int i = 1; i <= digits.size(); i++) {			// determin how many digits to use
+			for (int j = 0; j <= digits.size() - i; j++) {	// determin the starting position of product calculate
+				int product = 1;
+				int count = 0;
+				int k = j;
+				while(count < i) {
+					product *= digits.get(k++);
+					count++;
+				}
+				System.out.print(product + " ");
+				if(!set.contains(product)) {
+					set.add(product);
+				} else {
+					return false;
+				}
 			}
 		}
+		
 		
 		return true;
 	}
--- src/com/practice/WellOrderedString.java
+++ src/com/practice/WellOrderedString.java
@@ -16,18 +16,19 @@ public class WellOrderedString {
 	
 	//generate WellOrderedString
 	public static void generateWOS(int n, char start, StringBuilder sb) {
-		if(sb.length() == n) {
+		if(sb.length() == n) {						//when sb.length() == n then we hava a valid result
 			System.out.println(sb.toString());
 			return;
 		}
 		
-		for(char c = start; c <= 'z'; c++) {
+		for(char c = start; c <= 'z'; c++) {		//from char start to start 
 			sb.append(c);
 			generateWOS(n, (char)(c+1), sb);
 			sb.deleteCharAt(sb.length() - 1);
 			
 			sb.append((char)(c-32));
-			generateWOS(n, (char)(c+1), sb);
+			generateWOS(n, (char)(c+1), sb);		//!!!start from the same char as (char)c+1;
+													//not (char)c+32 or sth
 			sb.deleteCharAt(sb.length() - 1);
 		}
 	}
